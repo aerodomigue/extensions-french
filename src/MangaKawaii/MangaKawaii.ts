0155ac3ec38683539500541edb5dbc7d9c47d21f
aerodomigue <anthony.alcouffe@ynov.com>
2021-05-19 12:37:08 +0200

    Add Chapitre Detail

--- src/MangaKawaii/MangaKawaii.ts
+++ src/MangaKawaii/MangaKawaii.ts
@@ -19,7 +19,7 @@ import {
   const method = 'GET'
   
   export const MangaKawaiiInfo: SourceInfo = {
-    version: '0.1.13',
+    version: '0.1.14',
     name: 'MangaKawaii',
     icon: 'icon.png',
     author: 'aerodomigue',

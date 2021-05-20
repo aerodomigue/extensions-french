eb0377c6ad35428a150693a8e8223185d4c2c257
aerodomigue <anthony.alcouffe@ynov.com>
2021-05-21 00:03:33 +0200

    change version

--- src/MangaKawaii/MangaKawaii.ts
+++ src/MangaKawaii/MangaKawaii.ts
@@ -18,7 +18,7 @@ import {
   const method = 'GET'
   
   export const MangaKawaiiInfo: SourceInfo = {
-    version: 'Dev:0.1.26',
+    version: 'Dev:0.1.27',
     name: 'MangaKawaii',
     icon: 'icon.png',
     author: 'aerodomigue',
